b20ee8d4586b18d507b158e8766c37f7fc294335
qamarabdiaziz <tushez98@gmail.com>
2023-06-03 10:58:03 +0300

    initial commit

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costumes < ActiveRecord::Base
+
+end
---
+++ db/migrate/20230603042124_create_costumes_stores.rb
@@ -0,0 +1,14 @@
+class CreateCostumesStores < ActiveRecord::Migration[6.1]
+  def change
+     create_table :costumes_store do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :is_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+     end
+  end
+end
